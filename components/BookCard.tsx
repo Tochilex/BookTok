d1cbbded473d3653e911439fffb68a26eb9e54bc
Tochukwu <towunwanne@deloitte.com.ng>
2026-03-09 18:58:32 +0100

    Setup the app and implement and new book UI

---
+++ components/BookCard.tsx
@@ -0,0 +1,27 @@
+import { BookCardProps } from '@/types'
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+const BookCard = ({ title, author, coverURL, slug}: BookCardProps) => {
+  return (
+    <Link href={`/books/${slug}`}>
+        
+        {/* An article is like a standalone UI card to display information and figure tag shows what's in ite article */}
+        <article className='book-card'>
+            <figure className='book-card-figure'>
+                <div className='book-card-cover-wrapper'>
+                    <Image src={coverURL} alt={title} width={133} height={200} className='book-card-cover'/>
+                </div>
+            </figure>
+
+            <figcaption className='book-card-meta'>
+                <h3 className='book-card-title'>{title}</h3>
+                <p className='book-card-author'>{author}</p>
+            </figcaption>
+        </article>
+    </Link>
+  )
+}
+
+export default BookCard
