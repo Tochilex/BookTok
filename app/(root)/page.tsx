7a6a725b8046683770dc57eb5f348a1ffead9124
Tochukwu <towunwanne@deloitte.com.ng>
2026-03-18 11:44:00 +0100

    implement database setup and others

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,20 @@
 import BookCard from '@/components/BookCard'
 import HeroSection from '@/components/HeroSection'
-import { sampleBooks } from '@/lib/constants'
-import React from 'react'
-const Page = () => {
+import { getAllBooks } from '@/lib/actions/book.actions'
+//import { sampleBooks } from '@/lib/constants'
+
+
+
+const Page = async() => {
+  const bookResults = await getAllBooks();
+  const books = bookResults.success ? bookResults.data ?? [] : [];
+
   return (
     <main className='wrapper container'>
       <HeroSection />
 
       <div className='library-books-grid'>
-        {sampleBooks.map((book) => (
+        {books.map((book) => (
           <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug}  />
         ))}
       </div>
--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: { remotePatterns: [
-    { protocol: 'https', hostname: 'covers.openlibrary.org'}
+    { protocol: 'https', hostname: 'covers.openlibrary.org'},
+    { protocol: 'https', hostname: 'jbzjvlxrfay8c2wa.public.blob.vercel-storage.com'},
   ]}
 };
 
 export default nextConfig;
+
+
